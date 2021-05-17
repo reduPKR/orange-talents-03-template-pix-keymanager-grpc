fb2f4092e9df13164556615e2e5b15e1f385e83b
redu <reduardo19942@gmail.com>
2021-05-17 17:27:31 -0300

    020 consultas internas e externas iniciado

--- pix-server/src/main/kotlin/br/com/zup/pix/contaPix/GrpcExtensions.kt
+++ pix-server/src/main/kotlin/br/com/zup/pix/contaPix/GrpcExtensions.kt
@@ -1,10 +1,13 @@
 package br.com.zup.pix.contaPix.registra
 
+import br.com.zup.pix.ConsultarChavePixRequest
 import br.com.zup.pix.RegistrarChaveRequest
 import br.com.zup.pix.RemoverChaveRequest
 import br.com.zup.pix.contaPix.TipoChave
 import br.com.zup.pix.contaPix.TipoConta
 import br.com.zup.pix.contaPix.remove.RemoverChavePixRequest
+import io.micronaut.validation.validator.Validator
+import javax.validation.ConstraintViolationException
 
 fun RegistrarChaveRequest.toModel() : RegistrarChavePixRequest {
     return RegistrarChavePixRequest(
@@ -17,4 +20,21 @@ fun RegistrarChaveRequest.toModel() : RegistrarChavePixRequest {
 
 fun RemoverChaveRequest.toModel(): RemoverChavePixRequest {
     return RemoverChavePixRequest(clienteId,pixId)
+}
+
+fun ConsultarChavePixRequest.toModel(validator: Validator): Filtro {
+    val filtro = when(filtroCase){
+        PIXID -> let {
+            Filtro.PorPixId(it.clienteId, it.pixId)
+        }
+        CHAVE -> Filtro.PorChave(chave)
+        FILTRO_NOT_SET -> Filtro.invalido()
+    }
+
+    val validator = validator.validate(filtro)
+    if(validator.isNotEmpty()){
+        throw ConstraintViolationException(validator)
+    }
+
+    return filtro
 }
---
+++ pix-server/src/main/kotlin/br/com/zup/pix/contaPix/consulta/ConsultaChaveEndpoint.kt
@@ -0,0 +1,26 @@
+package br.com.zup.pix.contaPix.consulta
+
+import br.com.zup.pix.ConsultarChavePixRequest
+import br.com.zup.pix.ConsultarChavePixResponse
+import br.com.zup.pix.PixServerConsultarServiceGrpc
+import br.com.zup.pix.contaPix.ChavePixRepository
+import br.com.zup.pix.contaPix.registra.toModel
+import br.com.zup.pix.externo.bancoCentral.BancoCentralCliente
+import io.grpc.stub.StreamObserver
+import io.micronaut.validation.validator.Validator
+import javax.inject.Inject
+import javax.inject.Singleton
+
+@Singleton
+class ConsultaChaveEndpoint(
+    @Inject private val repository: ChavePixRepository,
+    @Inject private val bcCliente: BancoCentralCliente,
+    @Inject private val validator: Validator
+): PixServerConsultarServiceGrpc.PixServerConsultarServiceImplBase() {
+    override fun consultar(
+        request: ConsultarChavePixRequest,
+        responseObserver: StreamObserver<ConsultarChavePixResponse>
+    ) {
+        val filtro = request.toModel(validator)
+    }
+}
